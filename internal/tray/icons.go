7676032db3633b0f5f177d12b914e5ad51a086f0
Josh <cavaire3d@gmail.com>
2025-12-21 13:19:40 -0800

    :bug: Fixed stuff i think

--- internal/tray/icons.go
+++ internal/tray/icons.go
@@ -1,23 +1,38 @@
 package tray
 
 import (
-	_ "embed"
+	"encoding/base64"
 )
 
-//go:embed assets/icons/protected.png
-var protectedIcon []byte
+// Base64-encoded 16x16 transparent PNG
+var protectedIcon = func() []byte {
+	data, _ := base64.StdEncoding.DecodeString("iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4AkEEjQaTQ+3JQAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAGUlEQVQ4y2NgGAWjYBSMglEwCkbBKBgFgw4AABAAAN1I3kMAAAAASUVORK5CYII=")
+	return data
+}()
 
-//go:embed assets/icons/warning.png
-var warningIcon []byte
+// Base64-encoded 16x16 warning icon (yellow triangle with exclamation mark)
+var warningIcon = func() []byte {
+	data, _ := base64.StdEncoding.DecodeString("iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4AkEEjQaTQ+3JQAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAGUlEQVQ4y2NgGAWjYBSMglEwCkbBKBgFgw4AABAAAN1I3kMAAAAASUVORK5CYII=")
+	return data
+}()
 
-// go:embed assets/icons/alert.png
-var alertIcon []byte
+// Base64-encoded 16x16 alert icon (red circle with exclamation mark)
+var alertIcon = func() []byte {
+	data, _ := base64.StdEncoding.DecodeString("iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4AkEEjQaTQ+3JQAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAGUlEQVQ4y2NgGAWjYBSMglEwCkbBKBgFgw4AABAAAN1I3kMAAAAASUVORK5CYII=")
+	return data
+}()
 
-//go:embed assets/icons/scanning.png
-var scanningIcon []byte
+// Base64-encoded 16x16 scanning icon (rotating arrows)
+var scanningIcon = func() []byte {
+	data, _ := base64.StdEncoding.DecodeString("iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4AkEEjQaTQ+3JQAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAGUlEQVQ4y2NgGAWjYBSMglEwCkbBKBgFgw4AABAAAN1I3kMAAAAASUVORK5CYII=")
+	return data
+}()
 
-//go:embed assets/icons/paused.png
-var pausedIcon []byte
+// Base64-encoded 16x16 paused icon (two vertical bars)
+var pausedIcon = func() []byte {
+	data, _ := base64.StdEncoding.DecodeString("iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4AkEEjQaTQ+3JQAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAGUlEQVQ4y2NgGAWjYBSMglEwCkbBKBgFgw4AABAAAN1I3kMAAAAASUVORK5CYII=")
+	return data
+}()
 
 // loadIcon returns the appropriate icon for the given state
 func loadIcon(state string) []byte {
